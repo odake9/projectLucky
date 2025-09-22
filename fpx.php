798e57cced45e61cda72c716b84d96e6837d0c3a
yijielau <lauyijie0259@gmail.com>
2025-09-22 22:08:23 +0800

    UPDATE FUNCTION FPX

--- fpx.php
+++ fpx.php
@@ -1,5 +1,10 @@
 <?php
 $curl = curl_init();
+
+// Get total from payment.html (default 0 if not passed)
+$total = isset($_GET['total']) ? floatval($_GET['total']) : 0;
+$billAmount = $total * 100; // ToyyibPay expects cents
+
 $data = array(
     'userSecretKey'=>'kv5qqn3j-8e2w-s4u9-mn23-wg4ykvulqe0q',
     'categoryCode'=>'dvqe44uj',
@@ -7,21 +12,27 @@ $data = array(
     'billDescription'=>'Milk Tea Order',
     'billPriceSetting'=>1,
     'billPayorInfo'=>1,
-    'billAmount'=>200, // RM2.00
-    'billReturnUrl'=>'https://yourwebsite.com/payment_success.php',
-    'billCallbackUrl'=>'https://yourwebsite.com/callback.php',
-    'billExternalReferenceNo'=>'123456',
-    'billTo'=>'ADMIN',
+    'billAmount'=>$billAmount,
+    'billReturnUrl'=>'http://localhost/projectLucky/home.html',
+    'billCallbackUrl'=>'http://localhost/projectLucky/home.html',
+    'billExternalReferenceNo'=>uniqid(), // unique reference
+    'billTo'=>'LAU YI JIE',
     'billEmail'=>'lauyijie0259@email.com',
-    'billPhone'=>'0123456789'
+    'billPhone'=>'0178790259'
 );
+
 curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_URL, 'https://dev.toyyibpay.com/index.php/api/createBill');
+curl_setopt($curl, CURLOPT_URL, 'https://toyyibpay.com/index.php/api/createBill'); 
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 $result = curl_exec($curl);
 curl_close($curl);
 
 $obj = json_decode($result, true);
-header("Location: https://dev.toyyibpay.com/".$obj[0]['BillCode']);
+if (isset($obj[0]['BillCode'])) {
+    header("Location: https://toyyibpay.com/".$obj[0]['BillCode']);
+    exit();
+} else {
+    echo "Error creating bill: " . $result;
+}
 ?>
